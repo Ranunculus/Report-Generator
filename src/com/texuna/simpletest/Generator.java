afba017e83f0fa827c87f98caa3dc5c6a2b8d1d9
Ranunculus <tatinamalyutina@gmail.com>
2016-05-29 15:47:05 +0300

    init commit

---
+++ src/com/texuna/simpletest/Generator.java
@@ -0,0 +1,12 @@
+package com.texuna.simpletest;
+
+public class Generator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Generator");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+        // write your code here
+    }
+}
