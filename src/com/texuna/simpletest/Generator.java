34e7b8f8c61a9ea319562a36d3ca7e0517cfd2fd
Ranunculus <tatianamalyutina@gmail.com>
2016-06-01 18:51:25 +0300

    Added tsvParser and univocity-parsers library

--- src/com/texuna/simpletest/Generator.java
+++ src/com/texuna/simpletest/Generator.java
@@ -4,6 +4,7 @@ import com.texuna.simpletest.generators.ReportGeneratorFactory;
 import com.texuna.simpletest.generators.TxtReportGenerator;
 import com.texuna.simpletest.parsers.ParserFactory;
 import com.texuna.simpletest.parsers.XmlParser;
+import com.texuna.simpletest.parsers.TsvParser;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.stream.XMLStreamException;
@@ -17,7 +18,7 @@ import java.io.*;
  */
 public class Generator {
 
-    public static void main(String[] args) throws JAXBException, XMLStreamException, FileNotFoundException {
+    public static void main(String[] args) throws JAXBException, XMLStreamException, FileNotFoundException, UnsupportedEncodingException {
         /**
          * Creating xml parser to parse settings
          */
@@ -27,7 +28,8 @@ public class Generator {
         /**
          * Creating tsv parser to parse input data
          */
-        //todo
+        TsvParser tsvParser = (TsvParser) ParserFactory.getParser("tsv");
+        tsvParser.parse(args[1]);
 
         /**
          * Creating txtReportGenerator to create a report
--- src/com/texuna/simpletest/parsers/TsvParser.java
+++ src/com/texuna/simpletest/parsers/TsvParser.java
@@ -1,5 +1,13 @@
 package com.texuna.simpletest.parsers;
 
+import com.univocity.parsers.tsv.TsvParserSettings;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 /**
  * Created by Ranunculus on 29/05/16.
  */
@@ -7,7 +15,29 @@ public class TsvParser implements Parser{
 
     public static final String delimeter = "\t";
 
+    List<String[]> tsvParserResult;
     @Override
-    public void parse(String file) {
+    public void parse(String fileName) {
+        TsvParserSettings settings = new TsvParserSettings();
+        com.univocity.parsers.tsv.TsvParser parser = new com.univocity.parsers.tsv.TsvParser(settings);
+
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(fileName);
+            tsvParserResult = parser.parseAll(new InputStreamReader(fis, "UTF-16"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Data file not found");
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Unsupported Encoding");
+        }
+
+    }
+
+    public List<String[]> getTsvParserResult() {
+        return tsvParserResult;
+    }
+
+    public void setTsvParserResult(List<String[]> tsvParserResult) {
+        this.tsvParserResult = tsvParserResult;
     }
 }
